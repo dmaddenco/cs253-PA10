f5c7da89617e502af9ce1f5e20a9a3756077474b
David Madden <dmaddenco@gmail.com>
2016-12-04 21:49:05 -0700

    moved around some code

--- main.cpp
+++ main.cpp
@@ -25,31 +25,28 @@ int main(int argc, char* argv[]) {
 	ifstream inFile2(argv[2]);
 
 	Image training, test;
+    Perceptron perceptron;
+    Cluster cluster;
 
-	training.ReadFileNames(inFile1);
-	test.ReadFileNames(inFile2);
+    training.ReadFileNames(inFile1);
+    test.ReadFileNames(inFile2);
 
-	thread tH1(&Image::CreateHistograms, &training);
-	thread tH2(&Image::CreateHistograms, &test);
+    thread tH1(&Image::CreateHistograms, &training);
+    thread tH2(&Image::CreateHistograms, &test);
 
     tH1.join();
     tH2.join();
 
     thread tC1(&Image::CreateImages, &training);
-    thread tC2(&Image::CreateImages, &test);
-
     tC1.join();
-    tC2.join();
-
-	Perceptron perceptron;
 
     thread tP1(&Perceptron::GetClasses, &perceptron, training.images);
     tP1.join();
-
     thread tP2(&Perceptron::CreateNPerceptron, &perceptron);
     tP2.join();
 
-    Cluster cluster;
+    thread tC2(&Image::CreateImages, &test);
+    tC2.join();
 
     cluster.ClusterImages(test.images, atoi(argv[3]), perceptron.perceptrons);
 
